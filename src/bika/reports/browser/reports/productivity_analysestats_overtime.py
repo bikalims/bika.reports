a84b0e255ca33a9fef10f8123a1c3d10f02faa01
Mike Metcalfe <mike@webtide.co.za>
2025-06-07 12:45:20 +0200

    Black formatting only

--- src/bika/reports/browser/reports/productivity_analysestats_overtime.py
+++ src/bika/reports/browser/reports/productivity_analysestats_overtime.py
@@ -41,21 +41,20 @@ class Report(BrowserView):
         BrowserView.__init__(self, context, request)
         self.report = report
         self.headings = {
-            'header': _("Analysis turnaround times over time"),
-            'subheader': _("The turnaround time of analyses plotted over time")
+            "header": _("Analysis turnaround times over time"),
+            "subheader": _("The turnaround time of analyses plotted over time"),
         }
         self.formats = {
-            'columns': 2,
-            'col_heads': [
-                _('Date'),
-                 _('Turnaround time (h)')],
-            'class': ''
+            "columns": 2,
+            "col_heads": [_("Date"), _("Turnaround time (h)")],
+            "class": "",
         }
 
     def __call__(self):
         parms = []
-        query = dict(portal_type="Analysis", sort_on="getDateReceived",
-                     sort_order="ascending")
+        query = dict(
+            portal_type="Analysis", sort_on="getDateReceived", sort_order="ascending"
+        )
 
         # Filter by Service UID
         self.add_filter_by_service(query=query, out_params=parms)
@@ -68,11 +67,7 @@ class Report(BrowserView):
 
         # Period
         period = self.request.form.get("Period", "Day")
-        parms.append(
-            {"title": _("Period"),
-             "value": period,
-             "type": "text"}
-        )
+        parms.append({"title": _("Period"), "value": period, "type": "text"})
 
         # Fetch the data
         data_lines = []
@@ -84,15 +79,20 @@ class Report(BrowserView):
         analyses = api.search(query, CATALOG_ANALYSIS_LISTING)
         for analysis in analyses:
             analysis = api.get_object(analysis)
-            date_key = self.get_period_key(analysis.getDateReceived(),
-                                           self.date_format_short)
+            date_key = self.get_period_key(
+                analysis.getDateReceived(), self.date_format_short
+            )
             if date_key and date_key != prev_date_key:
                 if prev_date_key:
                     # Calculate averages
                     data_lines.append(
-                        [{"value": prev_date_key, 'class': ''},
-                         {"value": api.to_dhm_format(minutes=(duration//count)),
-                          "class": "number"}]
+                        [
+                            {"value": prev_date_key, "class": ""},
+                            {
+                                "value": api.to_dhm_format(minutes=(duration // count)),
+                                "class": "number",
+                            },
+                        ]
                     )
                 count = 0
                 duration = 0
@@ -107,9 +107,13 @@ class Report(BrowserView):
         if prev_date_key:
             # Calculate averages
             data_lines.append(
-                [{"value": prev_date_key, 'class': ''},
-                 {"value": api.to_dhm_format(minutes=(duration//count)),
-                  "class": "number"}]
+                [
+                    {"value": prev_date_key, "class": ""},
+                    {
+                        "value": api.to_dhm_format(minutes=(duration // count)),
+                        "class": "number",
+                    },
+                ]
             )
 
         # Totals
@@ -120,48 +124,55 @@ class Report(BrowserView):
             return self.generate_csv(data_lines)
 
         self.report_content = {
-            'headings': self.headings,
-            'parms': parms,
-            'formats': self.formats,
-            'datalines': data_lines,
-            'footings': [
-                [{'value': _('Total data points'), 'class': 'total'},
-                 {'value': total_count, 'class': 'total number'}],
-
-                [{'value': _('Average TAT'), 'class': 'total'},
-                 {'value': total_duration, 'class': 'total number'}]
-        ]}
-        return {'report_title': t(self.headings['header']),
-                'report_data': self.template()}
+            "headings": self.headings,
+            "parms": parms,
+            "formats": self.formats,
+            "datalines": data_lines,
+            "footings": [
+                [
+                    {"value": _("Total data points"), "class": "total"},
+                    {"value": total_count, "class": "total number"},
+                ],
+                [
+                    {"value": _("Average TAT"), "class": "total"},
+                    {"value": total_duration, "class": "total number"},
+                ],
+            ],
+        }
+        return {
+            "report_title": t(self.headings["header"]),
+            "report_data": self.template(),
+        }
 
     def add_filter_by_service(self, query, out_params):
         if not self.request.form.get("ServiceUID", ""):
             return
         query["getServiceUID"] = self.request.form["ServiceUID"]
         service = api.get_object_by_uid(query["getServiceUID"])
-        out_params.append({
-            "title": _("Analysis Service"),
-            "value": service.Title(),
-            "type": "text"})
+        out_params.append(
+            {"title": _("Analysis Service"), "value": service.Title(), "type": "text"}
+        )
 
     def add_filter_by_analyst(self, query, out_params):
         if not self.request.form.get("Analyst", ""):
             return
         query["getAnalyst"] = self.request.form["Analyst"]
-        out_params.append({
-            "title": _("Analyst"),
-            "value": self.user_fullname(query["getAnalyst"]),
-            "type": "text"})
+        out_params.append(
+            {
+                "title": _("Analyst"),
+                "value": self.user_fullname(query["getAnalyst"]),
+                "type": "text",
+            }
+        )
 
     def add_filter_by_instrument(self, query, out_params):
         if not self.request.form.get("getInstrumentUID", ""):
             return
         query["getInstrumentUID"] = self.request.form["getInstrumentUID"]
         instrument = api.get_object_by_uid(query["getInstrumentUID"])
-        out_params.append({
-            "title": _("Instrument"),
-            "value": instrument.Title(),
-            "type": "text"})
+        out_params.append(
+            {"title": _("Instrument"), "value": instrument.Title(), "type": "text"}
+        )
 
     def add_filter_by_date_range(self, query, out_params):
         date_query = formatDateQuery(self.context, "tats_DateReceived")
@@ -169,15 +180,17 @@ class Report(BrowserView):
             return
         query["getDateReceived"] = date_query
         out_params.append(
-            {"title": _("Received"),
-             "value": formatDateParms(self.context, "Tats_DateReceived"),
-             "type": "text"}
+            {
+                "title": _("Received"),
+                "value": formatDateParms(self.context, "Tats_DateReceived"),
+                "type": "text",
+            }
         )
 
     def get_period_key(self, date_time, date_format):
         period = self.request.form.get("Period", "Day")
         if period == "Day":
-            return date_time.strftime('%d %b %Y')
+            return date_time.strftime("%d %b %Y")
         elif period == "Week":
             day_of_week = date_time.strftime("%w")
             first_day = date_time - (int(day_of_week) - 1)
@@ -191,23 +204,26 @@ class Report(BrowserView):
 
     def generate_csv(self, data_lines):
         fieldnames = [
-            'Date',
-            'Turnaround time (h)',
+            "Date",
+            "Turnaround time (h)",
         ]
         output = StringIO()
-        dw = csv.DictWriter(output, extrasaction='ignore',
-                            fieldnames=fieldnames)
+        dw = csv.DictWriter(output, extrasaction="ignore", fieldnames=fieldnames)
         dw.writerow(dict((fn, fn) for fn in fieldnames))
         for row in data_lines:
-            dw.writerow({
-                'Date': row[0]['value'],
-                'Turnaround time (h)': row[1]['value'],
-            })
+            dw.writerow(
+                {
+                    "Date": row[0]["value"],
+                    "Turnaround time (h)": row[1]["value"],
+                }
+            )
         report_data = output.getvalue()
         output.close()
         date = datetime.datetime.now().strftime("%Y%m%d%H%M")
         setheader = self.request.RESPONSE.setHeader
-        setheader('Content-Type', 'text/csv')
-        setheader("Content-Disposition",
-                  "attachment;filename=\"analysesperservice_%s.csv\"" % date)
+        setheader("Content-Type", "text/csv")
+        setheader(
+            "Content-Disposition",
+            'attachment;filename="analysesperservice_%s.csv"' % date,
+        )
         self.request.RESPONSE.write(report_data)
