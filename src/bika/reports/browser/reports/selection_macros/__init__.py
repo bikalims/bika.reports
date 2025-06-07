ec117a3f729614e258eb67e47d2d8e19152680b0
Mike Metcalfe <mike@webtide.co.za>
2025-06-07 12:42:48 +0200

    Black formatting only

--- src/bika/reports/browser/reports/selection_macros/__init__.py
+++ src/bika/reports/browser/reports/selection_macros/__init__.py
@@ -32,7 +32,7 @@ def update_timer():
     """
     This function sets the time between updates of cached values
     """
-    return time() // (24 *60 * 60)
+    return time() // (24 * 60 * 60)
 
 
 def _cache_key_select_state(method, self, workflow_id, field_id, field_title):
@@ -51,8 +51,9 @@ def _cache_key_select_analysiscategory(method, self, style=None):
     return key
 
 
-def _cache_key_select_analysisservice(method, self, allow_blank,
-                                      multiselect, style=None):
+def _cache_key_select_analysisservice(
+    method, self, allow_blank, multiselect, style=None
+):
     """
     This function returns the key used to decide if method select_analysisservice has to be recomputed
     """
@@ -72,7 +73,7 @@ def _cache_key_select_analyst(method, self, allow_blank=False, style=None):
     """
     This function returns the key used to decide if method select_analyst has to be recomputed
     """
-    key = update_timer(),allow_blank, style
+    key = update_timer(), allow_blank, style
     return key
 
 
@@ -139,6 +140,7 @@ def _cache_key_select_supplier(method, self, style=None):
     key = update_timer(), style
     return key
 
+
 def _cache_key_select_reference_sample(method, self, style=None):
     """
     This function returns the key used to decide if method select_reference_sample has to be recomputed
@@ -155,7 +157,9 @@ def _cache_key_select_reference_service(method, self, style=None):
     return key
 
 
-def _cache_key_select_sample_type(method, self, allow_blank=True, multiselect=False, style=None):
+def _cache_key_select_sample_type(
+    method, self, allow_blank=True, multiselect=False, style=None
+):
     """
     This function returns the key used to decide if method select_sample_type has to be recomputed
     """
@@ -163,7 +167,9 @@ def _cache_key_select_sample_type(method, self, allow_blank=True, multiselect=Fa
     return key
 
 
-def _cache_key_select_groupingperiod(method, self, allow_blank=True, multiselect=False, style=None):
+def _cache_key_select_groupingperiod(
+    method, self, allow_blank=True, multiselect=False, style=None
+):
     """
     This function returns the key used to decide if method select_groupingperiod has to be recomputed
     """
@@ -180,7 +186,7 @@ def _cache_key_select_output_format(method, self, style=None):
 
 
 class SelectionMacrosView(BrowserView):
-    """ Display snippets for the query form, and
+    """Display snippets for the query form, and
         parse their results to contentFilter
 
     These methods are called directlly from tal:
@@ -206,26 +212,26 @@ class SelectionMacrosView(BrowserView):
         self.pc = self.portal_catalog
         self.rc = self.reference_catalog
 
-    select_analysiscategory_pt = ViewPageTemplateFile(
-        "select_analysiscategory.pt")
+    select_analysiscategory_pt = ViewPageTemplateFile("select_analysiscategory.pt")
 
     @ram.cache(_cache_key_select_analysiscategory)
     def select_analysiscategory(self, style=None):
         self.style = style
-        self.analysiscategories = self.bsc(portal_type='AnalysisCategory',
-                                           sort_on='sortable_title')
+        self.analysiscategories = self.bsc(
+            portal_type="AnalysisCategory", sort_on="sortable_title"
+        )
         return self.select_analysiscategory_pt()
 
     select_analysisservice_pt = ViewPageTemplateFile("select_analysisservice.pt")
 
     @ram.cache(_cache_key_select_analysisservice)
-    def select_analysisservice(self, allow_blank=True, multiselect=False,
-                               style=None):
+    def select_analysisservice(self, allow_blank=True, multiselect=False, style=None):
         self.style = style
         self.allow_blank = allow_blank
         self.multiselect = multiselect
-        self.analysisservices = self.bsc(portal_type='AnalysisService',
-                                         sort_on='sortable_title')
+        self.analysisservices = self.bsc(
+            portal_type="AnalysisService", sort_on="sortable_title"
+        )
         return self.select_analysisservice_pt()
 
     def parse_analysisservice(self, request):
@@ -237,21 +243,22 @@ class SelectionMacrosView(BrowserView):
             uids = [o.UID() for o in val]
             titles = [o.Title() for o in val]
             res = {}
-            res['contentFilter'] = ('getServiceUID', uids)
-            res['parms'] = {'title': _("Services"), 'value': ','.join(titles)}
-            res['titles'] = ','.join(titles)
+            res["contentFilter"] = ("getServiceUID", uids)
+            res["parms"] = {"title": _("Services"), "value": ",".join(titles)}
+            res["titles"] = ",".join(titles)
             return res
 
     select_analysisspecification_pt = ViewPageTemplateFile(
-        "select_analysisspecification.pt")
+        "select_analysisspecification.pt"
+    )
 
     @ram.cache(_cache_key_select_analysisspecification)
     def select_analysisspecification(self, style=None):
         self.style = style
         res = []
         bsc = getToolByName(self.context, "senaite_catalog_setup")
-        for s in bsc(portal_type='AnalysisSpec'):
-            res.append({'uid': s.UID, 'title': s.Title})
+        for s in bsc(portal_type="AnalysisSpec"):
+            res.append({"uid": s.UID, "title": s.Title})
         self.specs = res
         return self.select_analysisspecification_pt()
 
@@ -260,9 +267,9 @@ class SelectionMacrosView(BrowserView):
     @ram.cache(_cache_key_select_analyst)
     def select_analyst(self, allow_blank=False, style=None):
         self.style = style
-        self.analysts = getUsers(self.context,
-                                 ['Manager', 'Analyst', 'LabManager'],
-                                 allow_blank)
+        self.analysts = getUsers(
+            self.context, ["Manager", "Analyst", "LabManager"], allow_blank
+        )
         return self.select_analyst_pt()
 
     select_user_pt = ViewPageTemplateFile("select_user.pt")
@@ -279,8 +286,7 @@ class SelectionMacrosView(BrowserView):
     @ram.cache(_cache_key_select_client)
     def select_client(self, style=None):
         self.style = style
-        self.clients = self.pc(portal_type='Client',
-                               sort_on='sortable_title')
+        self.clients = self.pc(portal_type="Client", sort_on="sortable_title")
         return self.select_client_pt()
 
     def parse_client(self, request):
@@ -289,9 +295,9 @@ class SelectionMacrosView(BrowserView):
             obj = val and self.rc.lookupObject(val)
             title = obj.Title()
             res = {}
-            res['contentFilter'] = ('getClientUID', val)
-            res['parms'] = {'title': _("Client"), 'value': title}
-            res['titles'] = title
+            res["contentFilter"] = ("getClientUID", val)
+            res["parms"] = {"title": _("Client"), "value": title}
+            res["titles"] = title
             return res
 
     select_contact_pt = ViewPageTemplateFile("select_contact.pt")
@@ -299,8 +305,9 @@ class SelectionMacrosView(BrowserView):
     @ram.cache(_cache_key_select_contact)
     def select_contact(self, style=None):
         self.style = style
-        self.contacts = self.pc(portal_type='Contact', is_active=True,
-                                sort_on='sortable_title')
+        self.contacts = self.pc(
+            portal_type="Contact", is_active=True, sort_on="sortable_title"
+        )
         return self.select_contact_pt()
 
     select_daterange_pt = ViewPageTemplateFile("select_daterange.pt")
@@ -336,32 +343,38 @@ class SelectionMacrosView(BrowserView):
         return self._select_daterange(field_id, field_title, style)
 
     def parse_daterange(self, request, field_id, field_title):
-        from_date = request.get('%s_fromdate' % field_id, None)
-        from_date = from_date and from_date + ' 00:00' or None
-        to_date = request.get('%s_todate' % field_id, None)
-        to_date = to_date and to_date + ' 23:59' or None
+        from_date = request.get("%s_fromdate" % field_id, None)
+        from_date = from_date and from_date + " 00:00" or None
+        to_date = request.get("%s_todate" % field_id, None)
+        to_date = to_date and to_date + " 23:59" or None
         if from_date and to_date:
-            query = {'query': [from_date, to_date], 'range': 'min:max'}
+            query = {"query": [from_date, to_date], "range": "min:max"}
         elif from_date or to_date:
-            query = {'query': from_date or to_date,
-                     'range': from_date and 'min' or 'max'}
+            query = {
+                "query": from_date or to_date,
+                "range": from_date and "min" or "max",
+            }
         else:
             return None
 
         if from_date and to_date:
-            parms = translate(_("From ${start_date} to ${end_date}",
-                               mapping={"start_date":from_date, "end_date":to_date}))
+            parms = translate(
+                _(
+                    "From ${start_date} to ${end_date}",
+                    mapping={"start_date": from_date, "end_date": to_date},
+                )
+            )
         elif from_date:
-            parms = translate(_("Before ${start_date}",
-                               mapping={"start_date":from_date}))
+            parms = translate(
+                _("Before ${start_date}", mapping={"start_date": from_date})
+            )
         elif to_date:
-            parms = translate(_("After ${end_date}",
-                               mapping={"end_date":to_date}))
+            parms = translate(_("After ${end_date}", mapping={"end_date": to_date}))
 
         res = {}
-        res['contentFilter'] = (field_id, query)
-        res['parms'] = {'title': field_title, 'value': parms}
-        res['titles'] = parms
+        res["contentFilter"] = (field_id, query)
+        res["parms"] = {"title": field_title, "value": parms}
+        res["titles"] = parms
         return res
 
     select_instrument_pt = ViewPageTemplateFile("select_instrument.pt")
@@ -369,9 +382,9 @@ class SelectionMacrosView(BrowserView):
     @ram.cache(_cache_key_select_instrument)
     def select_instrument(self, style=None):
         self.style = style
-        self.instruments = self.bsc(portal_type='Instrument',
-                                    is_active=True,
-                                    sort_on='sortable_title')
+        self.instruments = self.bsc(
+            portal_type="Instrument", is_active=True, sort_on="sortable_title"
+        )
         return self.select_instrument_pt()
 
     select_period_pt = ViewPageTemplateFile("select_period.pt")
@@ -386,9 +399,9 @@ class SelectionMacrosView(BrowserView):
     @ram.cache(_cache_key_select_profile)
     def select_profile(self, style=None):
         self.style = style
-        self.analysisprofiles = self.bsc(portal_type='AnalysisProfile',
-                                         is_active=True,
-                                         sort_on='sortable_title')
+        self.analysisprofiles = self.bsc(
+            portal_type="AnalysisProfile", is_active=True, sort_on="sortable_title"
+        )
         return self.select_profile_pt()
 
     select_supplier_pt = ViewPageTemplateFile("select_supplier.pt")
@@ -396,20 +409,19 @@ class SelectionMacrosView(BrowserView):
     @ram.cache(_cache_key_select_supplier)
     def select_supplier(self, style=None):
         self.style = style
-        self.suppliers = self.bsc(portal_type='Supplier', is_active=True,
-                                  sort_on='sortable_title')
+        self.suppliers = self.bsc(
+            portal_type="Supplier", is_active=True, sort_on="sortable_title"
+        )
         return self.select_supplier_pt()
 
-    select_reference_sample_pt = ViewPageTemplateFile(
-        "select_reference_sample.pt")
+    select_reference_sample_pt = ViewPageTemplateFile("select_reference_sample.pt")
 
     @ram.cache(_cache_key_select_reference_sample)
     def select_reference_sample(self, style=None):
         self.style = style
         return self.select_reference_sample_pt()
 
-    select_reference_service_pt = ViewPageTemplateFile(
-        "select_reference_service.pt")
+    select_reference_service_pt = ViewPageTemplateFile("select_reference_service.pt")
 
     @ram.cache(_cache_key_select_reference_service)
     def select_reference_service(self, style=None):
@@ -426,7 +438,7 @@ class SelectionMacrosView(BrowserView):
         self.states = []
         for state_id in states:
             state = states[state_id]
-            self.states.append({'id': state.getId(), 'title': state.title})
+            self.states.append({"id": state.getId(), "title": state.title})
         return self.select_state_pt()
 
     @ram.cache(_cache_key_select_state)
@@ -443,9 +455,9 @@ class SelectionMacrosView(BrowserView):
         if val in states:
             state_title = states[val].title
             res = {}
-            res['contentFilter'] = (field_id, val)
-            res['parms'] = {'title': _('State'), 'value': state_title}
-            res['titles'] = state_title
+            res["contentFilter"] = (field_id, val)
+            res["parms"] = {"title": _("State"), "value": state_title}
+            res["titles"] = state_title
             return res
 
     select_sampletype_pt = ViewPageTemplateFile("select_sampletype.pt")
@@ -455,9 +467,9 @@ class SelectionMacrosView(BrowserView):
         self.style = style
         self.allow_blank = allow_blank
         self.multiselect = multiselect
-        self.sampletypes = self.bsc(portal_type='SampleType',
-                                    is_active=True,
-                                    sort_on='sortable_title')
+        self.sampletypes = self.bsc(
+            portal_type="SampleType", is_active=True, sort_on="sortable_title"
+        )
         return self.select_sampletype_pt()
 
     def parse_sampletype(self, request):
@@ -466,16 +478,15 @@ class SelectionMacrosView(BrowserView):
             obj = val and self.rc.lookupObject(val)
             title = obj.Title()
             res = {}
-            res['contentFilter'] = ('getSampleTypeUID', val)
-            res['parms'] = {'title': _("Sample Type"), 'value': title}
-            res['titles'] = title
+            res["contentFilter"] = ("getSampleTypeUID", val)
+            res["parms"] = {"title": _("Sample Type"), "value": title}
+            res["titles"] = title
             return res
 
     select_groupingperiod_pt = ViewPageTemplateFile("select_groupingperiod.pt")
 
     @ram.cache(_cache_key_select_groupingperiod)
-    def select_groupingperiod(self, allow_blank=True, multiselect=False,
-                              style=None):
+    def select_groupingperiod(self, allow_blank=True, multiselect=False, style=None):
         self.style = style
         self.allow_blank = allow_blank
         return self.select_groupingperiod_pt()
